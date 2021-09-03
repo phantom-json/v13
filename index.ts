dcd16d71eb03b90567c8d82623d3ee57d1b385b7
phantom-json <oliver.maule@outlook.com>
2021-09-04 00:53:15 +0100

    added slash commands 	modified:   index.ts 	modified:   package-lock.json 	modified:   package.json v0.02-dev added support for slash commands 	modified:   Change-Log.md 	new file:   Docs.md 	modified:   index.ts 	modified:   package-lock.json 	modified:   package.json v0.02-dev added support for slash commands 	modified:   Change-Log.md

--- index.ts
+++ index.ts
@@ -11,6 +11,63 @@ const client = new DiscordJS.Client({
 
 client.on('ready', () => {
     console.log(`the bot is ready`)
+
+    const guildId = "763349277493559296"
+    const guild = client.guilds.cache.get(guildId)
+    let commands
+
+    if (guild) {
+        commands = guild.commands
+    } else {
+        commands = client.application?.commands
+    }
+
+    commands?.create({
+        name: 'ping',
+        description: 'replies with pong'
+    })
+
+    commands?.create({
+        name: 'add',
+        description: ',dds two numbers',
+        options: [
+            {
+                name: 'num1',
+                description: 'the first number',
+                required: true,
+                type: DiscordJS.Constants.ApplicationCommandOptionTypes.NUMBER
+            },
+            {
+                name: 'num2',
+                description: 'the second number',
+                required: true,
+                type: DiscordJS.Constants.ApplicationCommandOptionTypes.NUMBER
+            }
+        ]
+    })
+})
+
+client.on('interactionCreate', async (Interaction) => {
+    if (!Interaction.isCommand()) {
+        return
+    }
+
+    const { commandName, options} = Interaction
+
+    if (commandName === 'ping') {
+        Interaction.reply({
+            content: 'pong',
+            ephemeral: true,
+        })
+    } else if (commandName === 'add') {
+        const num1 = options.getNumber('num1')!
+        const num2 = options.getNumber('num2')!
+
+        Interaction.reply({
+            content: `The sum is ${ num1 + num2 }`,
+            ephemeral: true
+        })
+    }
 })
 
 client.on('messageCreate', (message) => {
